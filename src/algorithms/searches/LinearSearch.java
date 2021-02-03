51adb8bbf3ea55b6c76ac14e5d600886d445875c
Konstantinos <kwstas654321@gmail.com>
2021-02-04 00:20:26 +0200

    LinearSearch

---
+++ src/algorithms/searches/LinearSearch.java
@@ -0,0 +1,13 @@
+package algorithms.searches;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinearSearch {
+    public static List<ArrayList> linearSearch(List<ArrayList> lists){
+        
+        
+        return null;
+    }
+
+}
