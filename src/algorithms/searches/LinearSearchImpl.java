f4c6a5836727a17876e94c6f93540783077930ec
Konstantinos <kwstas654321@gmail.com>
2021-02-04 13:35:31 +0200

    Linear Search

--- src/algorithms/searches/LinearSearchImpl.java
+++ src/algorithms/searches/LinearSearchImpl.java
@@ -4,20 +4,25 @@ import java.util.Random;
 import java.util.stream.Stream;
 
 public class LinearSearchImpl implements LinearSearch {
+
     Random r = new Random();
     int size = 500;
     int maxElement = 250;
 
     @Override
     public void linearSearch() {
-        Integer[] integers = fakeData();      
+        Integer[] integers = fakeData();
+        /**
+         * Print Array uncomment
+         */
+        //printArray(integers);
         int shouldBeFound = randomNumber(integers);
         SearchAlgorithmImpl search = new SearchAlgorithmImpl();
         int atIndex = search.find(integers, shouldBeFound);
         System.out.println(
-        String.format(
-            "Should be found: %d. Found %d at index %d. An array length %d",
-            shouldBeFound, integers[atIndex], atIndex, size));
+                String.format(
+                        "\nShould be found: %d. Found %d at index %d. An array length %d",
+                        shouldBeFound, integers[atIndex], atIndex, size));
 
     }
 
@@ -29,7 +34,20 @@ public class LinearSearchImpl implements LinearSearch {
     }
 
     private int randomNumber(Integer[] integers) {
-       return (integers[r.nextInt(size - 1)]);
+        return (integers[r.nextInt(size - 1)]);
+    }
+
+    public void printArray(Integer[] integers) {
+        int counter = 0;
+        
+        for (Integer number : integers) {            
+                System.out.print((counter++)+": "+number);
+                if(counter != integers.length)
+                    System.out.print(", ");
+                if( (counter % 10) == 0){
+                    System.out.println("");
+                }
+        }
     }
 
 }
