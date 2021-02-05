a156fe9d3b8c282c2316c5cc4cd2293fd334ce04
Konstantinos <kwstas654321@gmail.com>
2021-02-05 13:25:28 +0200

    Create SearchAlgorithm.java

---
+++ src/algorithms/searches/factory/SearchAlgorithm.java
@@ -0,0 +1,14 @@
+package algorithms.searches.factory;
+
+import models.enums.SearchAlgorithmType;
+
+ public interface SearchAlgorithm {
+
+    /**
+     * @param key is an element which should be found
+     * @param array is an array where the element should be found
+     * @param <T> Comparable type
+     * @return first found index of the element
+     */
+    <T extends Comparable<T>> int find(T array[], T key);  
+}
