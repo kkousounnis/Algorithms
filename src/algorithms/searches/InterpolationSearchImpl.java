e526489abd7d3b0733c220f9d8307451f4dd7425
Konstantinos <kwstas654321@gmail.com>
2021-02-17 00:16:51 +0200

    Update InterpolationSearchImpl.java

--- src/algorithms/searches/InterpolationSearchImpl.java
+++ src/algorithms/searches/InterpolationSearchImpl.java
@@ -1,5 +1,40 @@
 package algorithms.searches;
 
-public class InterpolationSearchImpl implements InterpolationSearch{
+/**
+ *
+ * Worst-case performance O(n) Best-case performance O(1) Average perfomance
+ * O(log(log(n)))
+ */
+public class InterpolationSearchImpl implements InterpolationSearch {
+
+    @Override
+    public int interPolationSearch(int array[], int key) {
+        int startIndex = 0;
+        int endIndex = (array.length - 1);
+
+        while (checkIndex(startIndex, endIndex, key, array)) {
+            int position = startIndex
+                    + (((endIndex - startIndex) / (array[endIndex] - array[startIndex]))
+                    * (key - array[startIndex]));
+
+            if (array[position] == key) {
+                return (position);
+            }
+
+            if (array[position] < key) {
+                startIndex = position + 1;
+            } else {
+                endIndex = position - 1;
+            }
+
+        }
+        return (-1);
+    }
+
+    private static boolean checkIndex(int startIndex, int endIndex,
+            int key, int[] array) {
+        return (startIndex <= endIndex
+                && key >= array[startIndex] && key <= array[endIndex]);
+    }
 
 }
