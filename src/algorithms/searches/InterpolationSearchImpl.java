e56d835cea992aa02b6cc5fddbb3bf04dcbe5633
Konstantinos <kwstas654321@gmail.com>
2021-02-17 01:31:10 +0200

    Update InterpolationSearchImpl.java

--- src/algorithms/searches/InterpolationSearchImpl.java
+++ src/algorithms/searches/InterpolationSearchImpl.java
@@ -8,20 +8,20 @@ package algorithms.searches;
 public class InterpolationSearchImpl implements InterpolationSearch {
 
     @Override
-    public <T extends Comparable<T>> int interPolationSearch(T[] array[], T key1) {
+    public int interPolationSearch(int array[], int key) {
         int startIndex = 0;
         int endIndex = (array.length - 1);
-        int key = Integer.parseInt((String)key1);
+
         while (checkIndex(startIndex, endIndex, key, array)) {
             int position = startIndex
                     + (((endIndex - startIndex) / (array[endIndex] - array[startIndex]))
                     * (key - array[startIndex]));
 
-            if (array[position].equals(key)) {
+            if (array[position] == key) {
                 return (position);
             }
-            int myvalue = Integer.parseInt((String)array[position]);
-            if (myvalue < key) {
+
+            if (array[position] < key) {
                 startIndex = position + 1;
             } else {
                 endIndex = position - 1;
@@ -31,9 +31,10 @@ public class InterpolationSearchImpl implements InterpolationSearch {
         return (-1);
     }
 
-    private <T extends Comparable<T>> boolean checkIndex(int startIndex, int endIndex,
+    private static boolean checkIndex(int startIndex, int endIndex,
             int key, int[] array) {
-        return (startIndex <= endIndex && key >= array[startIndex] && key <= array[endIndex]);
+        return (startIndex <= endIndex
+                && key >= array[startIndex] && key <= array[endIndex]);
     }
 
 }
