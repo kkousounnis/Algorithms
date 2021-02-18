ce4e6e9f510b713e6502a9dec23af3355b83fb3d
Konstantinos <kwstas654321@gmail.com>
2021-02-19 00:05:43 +0200

    Update JumpSearchImpl.java

--- src/algorithms/searches/JumpSearchImpl.java
+++ src/algorithms/searches/JumpSearchImpl.java
@@ -3,16 +3,29 @@ package algorithms.searches;
 /**
  * Worst-case performance O(√n)
  * Average perfomance O(√n)
- * Best-case performance O(1) 
+ * Best-case performance O(1)
  */
-
-public class JumpSearchImpl implements JumpSearch{
+public class JumpSearchImpl implements JumpSearch {
 
     @Override
     public <T extends Comparable<T>> int jumpSearch(T[] array, T value) {
-        int length = array.length
-        return (0);
+        int length = array.length;
+        int blockSize = (int) Math.sqrt(length);
+        /*block size to be jumped*/
+
+        int limit = blockSize;
+        while (value.compareTo(array[limit]) > 0 && limit < array.length - 1) {
+            limit = Math.min(limit + blockSize, array.length - 1);
+        }
+
+        for (int i = limit - blockSize; i <= limit; i++) {
+            if (array[i] == value) {
+                return (i);
+            }
+
+        }
+
+        return (-1);
     }
-    
-    
+
 }
