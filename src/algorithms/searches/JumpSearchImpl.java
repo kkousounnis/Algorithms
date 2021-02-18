5a6c86a43172ca818c7058a1293ff8674351e38c
Konstantinos <kwstas654321@gmail.com>
2021-02-18 23:51:34 +0200

    Update JumpSearchImpl.java

--- src/algorithms/searches/JumpSearchImpl.java
+++ src/algorithms/searches/JumpSearchImpl.java
@@ -1,9 +1,16 @@
 package algorithms.searches;
 
+/**
+ * Worst-case performance O(√n)
+ * Average perfomance O(√n)
+ * Best-case performance O(1) 
+ */
+
 public class JumpSearchImpl implements JumpSearch{
 
     @Override
     public <T extends Comparable<T>> int jumpSearch(T[] array, T value) {
+        int length = array.length
         return (0);
     }
     
