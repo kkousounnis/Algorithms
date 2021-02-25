41d9fff43db9918926fe85ed0d436d56957ee9f3
Konstantinos <kwstas654321@gmail.com>
2021-02-25 22:38:40 +0200

    Update QuickSort.java

--- src/algorithms/sorts/QuickSort.java
+++ src/algorithms/sorts/QuickSort.java
@@ -2,4 +2,7 @@ package algorithms.sorts;
 
 public interface QuickSort {
 
+    public <T extends Comparable<T>> T[] partition(T arr[], int low, int high);
+
+    public <T extends Comparable<T>> T[] quickSort(T arr[], int low, int high);
 }
